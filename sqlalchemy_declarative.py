1a3d8d4915ecc41244e64b4521454cc5a6240962
lenok <lenok@server1.example.com>
2014-09-17 18:01:01 +0300

    script to create database

---
+++ sqlalchemy_declarative.py
@@ -0,0 +1,18 @@
+import os
+import sys
+from sqlalchemy import Column, ForeignKey, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import relationship
+from sqlalchemy import create_engine
+ 
+Base = declarative_base()
+ 
+class DownStorage(Base):
+    __tablename__ = 'downloads'
+    id = Column(Integer, primary_key=True)
+    url = Column(String(250), nullable=False)
+    status = Column(String(250), nullable=False)
+ 
+engine = create_engine('sqlite:///downloads_storage.db')
+ 
+Base.metadata.create_all(engine)
